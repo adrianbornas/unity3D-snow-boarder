b285f814c32035930ca37e33725152484972b892
adrianbornas <adrian.bornas@gmail.com>
2022-06-13 18:37:43 +0200

    Moved sampleScene to Level1. Reload scene when player wins and looses

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == "Ground") {
-            Debug.Log("Ouch!");
+            SceneManager.LoadScene(0);
         }
     }
 }
