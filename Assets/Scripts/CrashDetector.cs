8c7cf6fdb98bb5a19dc5d421ed8091cf116188a2
adrianbornas <adrian.bornas@gmail.com>
2022-07-07 16:45:09 +0200

    Trigger particle effect on character death and finish line

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    [SerializeField] float deathLoadTime = 0.5f;
+    [SerializeField] float deathLoadTime = 0.2f;
+    [SerializeField] ParticleSystem crassEffect;
 
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == "Ground") {
+            crassEffect.Play();
             Invoke("ReloadScene", deathLoadTime);
         }
     }
