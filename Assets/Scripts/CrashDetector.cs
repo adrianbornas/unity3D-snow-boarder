ae67cf74d75172ca091ceaba24198f5f244b3c73
adrianbornas <adrian.bornas@gmail.com>
2022-07-07 16:48:15 +0200

    Typo error

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,11 +6,11 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float deathLoadTime = 0.2f;
-    [SerializeField] ParticleSystem crassEffect;
+    [SerializeField] ParticleSystem crashEffect;
 
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == "Ground") {
-            crassEffect.Play();
+            crashEffect.Play();
             Invoke("ReloadScene", deathLoadTime);
         }
     }
